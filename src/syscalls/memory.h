8de13d866ea823f82f5d7041392dd5b1217cb51e
David Given <dg@cowlark.com>
2010-03-13 01:17:41 +0000

    Added forgotten file.

---
+++ src/syscalls/memory.h
@@ -0,0 +1,11 @@
+/* © 2010 David Given.
+ * LBW is licensed under the MIT open source license. See the COPYING
+ * file in this distribution for the full text.
+ */
+
+#ifndef SYSCALLS_MEMORY_H
+#define SYSCALLS_MEMORY_H
+
+extern void ClearBrk();
+
+#endif
