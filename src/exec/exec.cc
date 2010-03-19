bb78636930a0779a2926d5b70901aede9272ea8a
David Given <dg@cowlark.com>
2010-03-19 22:43:41 +0000

    Added first draft shell script support.

--- src/exec/exec.cc
+++ src/exec/exec.cc
@@ -5,6 +5,7 @@
 
 #include "globals.h"
 #include "exec/ElfLoader.h"
+#include "exec/exec.h"
 #include "filesystem/FD.h"
 #include "filesystem/VFS.h"
 #include "filesystem/InterixVFSNode.h"
@@ -13,7 +14,8 @@ enum
 {
 	UNKNOWN_EXECUTABLE,
 	ELF_EXECUTABLE,
-	INTERIX_EXECUTABLE
+	INTERIX_EXECUTABLE,
+	SHELL_EXECUTABLE
 };
 
 static int probe_executable(const string& filename)
@@ -29,6 +31,8 @@ static int probe_executable(const string& filename)
 		return ELF_EXECUTABLE;
 	if (memcmp(buffer, "MZ", 2) == 0)
 		return INTERIX_EXECUTABLE;
+	if (memcmp(buffer, "#!", 2) == 0)
+		return SHELL_EXECUTABLE;
 	return UNKNOWN_EXECUTABLE;
 }
 
@@ -80,6 +84,81 @@ static void linux_exec(const string& pathname, const char* argv[], const char* e
 
 	//log("exec <%s> into pid %d", pathname.c_str(), getpid());
 	execve(Options.LBW.c_str(), (char* const*) argv, (char* const*) newenviron);
+}
+
+static bool isterm(int c)
+{
+	return (c == '\0') || (c == '\n') || (c == '\r');
+}
+
+static bool issep(int c)
+{
+	return isterm(c) || (c == ' ') || (c == '\t');
+}
+
+static void shell_exec(const string& pathname, const char* argv[], const char* environ[])
+{
+	Ref<FD> ref = VFS::OpenFile(NULL, pathname);
+	int fd = ref->GetRealFD();
+
+	/* We know this will work, because we've just tried it when probing the
+	 * file.
+	 */
+
+	char buffer[128];
+	if (pread(fd, buffer, sizeof(buffer), 0) == -1)
+		throw EIO;
+	buffer[sizeof(buffer)-1] = '\0';
+
+	/* Messy code to parse the #! line. */
+
+	char* interpreter_start = buffer + 2;
+	while (issep(*interpreter_start))
+		interpreter_start++;
+	if (!*interpreter_start)
+		throw EINVAL;
+
+	char* interpreter_end = interpreter_start + 1;
+	while (!issep(*interpreter_end))
+		interpreter_end++;
+
+	char* arg_start = NULL;
+	if (!isterm(*interpreter_end))
+	{
+		arg_start = interpreter_end + 1;
+
+		while (issep(*arg_start))
+			arg_start++;
+
+		if (isterm(*arg_start))
+			arg_start = NULL;
+
+		char* arg_end = arg_start + 1;
+
+		while (!issep(*arg_end))
+			arg_end++;
+		*arg_end = '\0';
+	}
+
+	*interpreter_end = '\0';
+
+	/* Create a new argument array with this new arguments on the front. */
+
+	int argc = 0;
+	while (argv[argc])
+		argc++;
+
+	const char* newargv[argc+3];
+	int index = 0;
+	newargv[index++] = interpreter_start;
+	if (arg_start)
+		newargv[index++] = arg_start;
+	memcpy(newargv+index, argv, (argc+1) * sizeof(char*));
+	newargv[index] = pathname.c_str();
+
+	/* Retry the exec. */
+
+	Exec(newargv[0], newargv, environ);
 }
 
 void Exec(const string& pathname, const char* argv[], const char* environ[])
@@ -141,6 +220,9 @@ void Exec(const string& pathname, const char* argv[], const char* environ[])
 			case INTERIX_EXECUTABLE:
 				execve(pathname.c_str(), (char* const*) argv, (char* const*) environ);
 				break;
+
+			case SHELL_EXECUTABLE:
+				shell_exec(pathname, argv, environ);
 		}
 
 		throw errno;
